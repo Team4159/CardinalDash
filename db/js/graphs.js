69c091305d5f74722c2d8abdc93cc6bdde252aad
Brandon Lou <brandon@Brandons-Mac.local>
2017-01-28 20:41:41 -0800

    Add json parsing

--- db/js/graphs.js
+++ db/js/graphs.js
@@ -1,5 +1,5 @@
 /* dependencies */
-const plotly = require('plotly.js'),
+const Plotly = require('plotly.js'),
       $ = require('jquery'),
       app = require('electron').remote,
       dialog = app.dialog,
@@ -8,29 +8,37 @@ const plotly = require('plotly.js'),
 /* load templates */
 $('#nav').load("templates/nav.html");
 
-$('#load-btn').click(function() {
 
+$('#load-btn').click(function() {
   dialog.showOpenDialog(function (fileNames) {
     // fileNames is an array that contains all the selected
    if(fileNames === undefined){
     console.log("No file selected");
    } else {
-    readFile(fileNames[0]);
+    //readFile(fileNames[0]);
+    makeGraph(fileNames[0]);
    }
   });
 });
 
+// function readFile(filepath){
+//   fs.readFile(filepath, 'utf-8', function (err, data) {
+//     if(err){
+//       alert("An error ocurred reading the file :" + err.message);
+//       return;
+//     }
+//     makeGraphs(data);
+//   });
+// }
+//
+// var everything;
+//
+// function makeGraphs(data) {
+//   everything = JSON.parse(JSON.stringify(data));
+// }
 
-function readFile(filepath){
-  fs.readFile(filepath, 'utf-8', function (err, data) {
-    if(err){
-      alert("An error ocurred reading the file :" + err.message);
-      return;
-    }
-    makeGraphs(data);
+function makeGraph(jsonFile) {
+  Plotly.d3.json(jsonFile, function(figure){
+    console.log(figure);
   });
-}
-
-function makeGraphs(data) {
-  console.log(data);
 }
