d80b2cabe1059d482bdb2601edc8ccba472ccc1e
Cole Scott <colescottsf@gmail.com>
2017-02-28 14:48:47 -0800

    Update some stuff

--- dash/ui/dashboard/index.js
+++ dash/ui/dashboard/index.js
@@ -8,6 +8,8 @@ import { connect } from "react-redux";
 import * as a from "../../store/actions.js";
 import * as s from "../../store/selectors.js";
 
+var maxTotalCurrent = 0;
+
 var maxCurrent = Array(16);
 maxCurrent.fill(0);
 
@@ -15,20 +17,31 @@ const Dashboard = (props) => {
     const connect = () => props.connect(props.form.addressInput);
     const disconnect = () => props.disconnect();
 
-    for (let i = 0;i < 16;i++)
-        if (props.data.PDP.Current[ i ] > maxCurrent[ i ])
-            maxCurrent[ i ] = props.data.PDP.Current[ i ];
+    if (props.data.PDP) {
+        for (let i = 0;i < 16;i++)
+            if (props.data.PDP.Current[ i ] > maxCurrent[ i ])
+                maxCurrent[ i ] = props.data.PDP.Current[ i ];
+        if (getCurrent(props.data) > maxTotalCurrent)
+            maxTotalCurrent = getCurrent(props.data);
+    }
 
-    const listItems = Array(16);
+    const listItems = Array(17);
     for (let i = 0;i < 16;i++)    {
         listItems[ i ] = (
-            <tr>
-                <td>{i}</td>
-                <td>{getCurrentPort(props.data, i)}</td>
-                <td>{getMaxCurrentPort(props.data, i)}</td>
+            <tr key={i}>
+                <td key={i * 3}>{i}</td>
+                <td key={i * 3 + 1}>{getCurrentPort(props.data, i)}</td>
+                <td key={i * 3 + 2}>{getMaxCurrentPort(props.data, i)}</td>
             </tr>
         );
     }
+    listItems[ 16 ] = (
+        <tr key={16}>
+            <td>Total</td>
+            <td>{getCurrent(props.data)}</td>
+            <td>{getMaxCurrent()}</td>
+        </tr>
+    );
 
     return (
         <Container>
@@ -62,28 +75,27 @@ const Dashboard = (props) => {
                     <Panel header="Robot Status">
                         <ListGroup fill>
                             <ListGroupItem>Voltage: {getVoltage(props.data)}V</ListGroupItem>
-                            <ListGroupItem>Current: {getCurrent(props.data)}A</ListGroupItem>
                             <ListGroupItem>Mode: {getState(props.data)}</ListGroupItem>
                             <ListGroupItem>State: {getEnabled(props.data) ? "Enabled" : "Disabled"}</ListGroupItem>
                         </ListGroup>
                     </Panel>
                     <Panel header="Pistons">
                         <ListGroup fill>
+                            <ListGroupItem>Item 0</ListGroupItem>
                             <ListGroupItem>Item 1</ListGroupItem>
                             <ListGroupItem>Item 2</ListGroupItem>
-                            <ListGroupItem></ListGroupItem>
                         </ListGroup>
                     </Panel>
                 </Col>
 
                 <Col sm={2}>
                     <Panel header="Current">
-                        <Table striped bordered condensed hover>
+                        <Table striped bordered condensed hover responsive>
                             <thead>
                                 <tr>
-                                    <th>Port</th>
-                                    <th>Current</th>
-                                    <th>Peak</th>
+                                    <th key={0}>Port</th>
+                                    <th key={1}>Current</th>
+                                    <th key={2}>Peak</th>
                                 </tr>
                             </thead>
                             <tbody>
@@ -113,12 +125,12 @@ const headerMessage = (props) => {
 };
 
 const getVoltage = (data) => {
-    if (data)
+    if (data.PDP)
         return Number(data.PDP.Voltage).toFixed(2);
 };
 
 const getCurrent = (data) => {
-    if (data)    {
+    if (data.PDP)    {
         let current = 0;
         for (let i = 0;i < 16;i++)
             current += data.PDP.Current[ i ];
@@ -127,18 +139,23 @@ const getCurrent = (data) => {
     }
 };
 
+const getMaxCurrent = () => {
+    return Number(maxTotalCurrent).toFixed(2);
+};
+
+
 const getCurrentPort = (data, port) => {
-    if (data)
+    if (data.PDP)
         return Number(data.PDP.Current[ port ]).toFixed(2);
 };
 
 const getMaxCurrentPort = (data, port) => {
-    if (data)
+    if (data.PDP)
         return Number(maxCurrent[ port ]).toFixed(2);
 };
 
 const getState = (data) => {
-    if (data)
+    if (data.RoboRIO)
     switch (data.RoboRIO.Mode) {
         case 0:
             return "Disabled";
@@ -154,7 +171,7 @@ const getState = (data) => {
 };
 
 const getEnabled = (data) => {
-    if (data)
+    if (data.RoboRIO)
         return data.RoboRIO.Enabled;
 };
 
--- dash/ui/logs/index.js
+++ dash/ui/logs/index.js
@@ -1,17 +1,72 @@
 import React from "react";
 import Container from "../container";
 
-import { Col } from "react-bootstrap";
+import { Button, Col, ControlLabel, FormControl, FormGroup, HelpBlock } from "react-bootstrap";
+import { remote } from "electron";
+//const { dialog } = require("electron").remote;
+
+
+import createPlotlyComponent from "react-plotlyjs";
+import Plotly from "plotly.js/dist/plotly-cartesian";
+const PlotlyComponent = createPlotlyComponent(Plotly);
 
 const Logs = () => {
+    let data = [
+        {
+            type: "lines",
+            x: [1, 2, 3],
+            y: [6, 2, 3],
+            name: "Port 0"
+        },
+        {
+            type: "lines",
+            x: [1, 2, 3],
+            y: [8, 19, 4],
+            name: "Port 1"
+        }
+    ];
+    let layout = {
+        autosize: true,
+        title: "Current",
+        xaxis: {
+            title: "time"
+        },
+        yaxis: {
+            title: "current (A)"
+        },
+        font: {
+            family: '"Lato","Helvetica Neue",Helvetica,Arial,sans-serif',
+            color: "#ebebeb"
+        },
+        paper_bgcolor: "#4e5d6c"
+        //plot_bgcolor: 'rgba(0,0,0,0)'
+    };
+    let config = {
+        showLink: false,
+        displayModeBar: false
+    };
     return (
         <Container>
-            <Col sm={12}>
+            <Col sm={12} className="text-left fill">
                 <h2>Pretty graphs</h2>
-                <button id="load-btn" type="button" className="btn btn-default">Load JSON file</button>
+                <form>
+                    <Button onClick={() => { console.log(remote.dialog.showOpenDialog({properties: ['openFile']})) }}>
+                        Open Log File
+                    </Button>
+                </form>
+
+                <PlotlyComponent className="currentPlot" data={data} layout={layout} config={config}/>
             </Col>
         </Container>
-  );
+    );
+  //   return (
+  //       <Container>
+  //           <Col sm={12}>
+  //               <h2>Pretty graphs</h2>
+  //               <button id="load-btn" type="button" className="btn btn-default">Load JSON file</button>
+  //           </Col>
+  //       </Container>
+  // );
 };
 
 export default Logs;
